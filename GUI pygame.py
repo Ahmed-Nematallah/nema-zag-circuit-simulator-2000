f6c2909e70b516f0335f3bc4a525438fa8fa16f6
ahmed zaghloul <zaghloulahmed55@yahoo.com>
2016-12-06 00:42:00 +0200

    test

--- GUI pygame.py
+++ GUI pygame.py
@@ -33,11 +33,12 @@ pygame.init ()
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("circuit sim")
 gameExit =False
-drawing = False
+drawing = True
 drawcursx =0
 drawcursy = 0
 cursx = 0
 cursy = 0
+lines=[]
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -46,14 +47,26 @@ while not gameExit:
             cursx,cursy = snapToGrid(event.pos[0],event.pos[1],30)
             print([cursx,cursy,cursx+10,cursy+10])
         if event.type == pygame.MOUSEBUTTONUP:
+            print("mouse down")
+            drawcursx = cursx
+            drawcursy = cursy
             if drawing == False:
                 drawing = True
-            if drawing == True:
+            elif drawing == True:
+                lines.append([drawcursx,drawcursy,cursx,cursy])
                 drawing = False
     gameDisplay.fill((150,150,150))
+    #draw current line
+    if drawing:
+        if drawcursx == cursx or drawcursy == cursy:
+            pygame.draw.line(gameDisplay, (0,255,255), [drawcursx,drawcursy], [cursx,cursy],3)
+    #render drawn lines
+    if len(lines)>0:
+        for l in lines :
+            pygame.draw.line(gameDisplay, (0,255,255), [l[0],l[1]],[l[2],l[3]],3)
     #pygame.draw.rect(gameDisplay, (0,255,255), [cursx-10,cursy-10,cursx+10,cursy+10])
     #pygame.draw.rect(gameDisplay, (0,255,255), [320,130,280,170])
-    
-    pygame.draw.line(gameDisplay, (0,255,255), [0,0], [cursx,cursy])
+
+    #pygame.draw.line(gameDisplay, (0,255,255), [0,0], [cursx,cursy])
     pygame.display.update()
 pygame.quit()
