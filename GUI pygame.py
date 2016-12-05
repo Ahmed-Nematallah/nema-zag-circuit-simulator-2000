0395d6da59e94b9aa0c6e0bd05ac811441991fa1
Ahmed Nematallah <ahmed_nematallah@hotmail.com>
2016-12-06 01:14:10 +0200

    Fixed Wires

--- GUI pygame.py
+++ GUI pygame.py
@@ -17,40 +17,48 @@ gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("circuit sim")
 gameExit =False
 drawing = False
-drawcursx =0
-drawcursy = 0
-cursx = 0
-cursy = 0
+drawgridX =0
+drawgridY = 0
+gridX = 0
+gridY = 0
 lines=[]
+verticalWire = False
 
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.MOUSEMOTION:
-            cursx,cursy = snapToGrid(event.pos[0],event.pos[1],30)
-            print([cursx,cursy,cursx+10,cursy+10])
+            gridX,gridY = snapToGrid(event.pos[0],event.pos[1],30)
+            print([gridX,gridY,gridX+10,gridY+10])
         if event.type == pygame.MOUSEBUTTONUP:
             print("mouse down")
             if drawing == False:
-                drawcursx = cursx
-                drawcursy = cursy
+                drawgridX = gridX
+                drawgridY = gridY
                 drawing = True
             elif drawing == True:
-                lines.append([drawcursx,drawcursy,cursx,cursy])
+                if (verticalWire):
+                    lines.append([drawgridX,drawgridY,drawgridX,gridY])
+                else:
+                    lines.append([drawgridX,drawgridY,gridX,drawgridY])
                 drawing = False
     gameDisplay.fill((150,150,150))
     #draw current line
     if drawing:
-        if drawcursx == cursx or drawcursy == cursy:
-            pygame.draw.line(gameDisplay, (0,255,255), [drawcursx,drawcursy], [cursx,cursy],3)
+        if abs(drawgridX - gridX) >= abs(drawgridY - gridY):
+            verticalWire = False
+            pygame.draw.line(gameDisplay, (0,255,255), [drawgridX,drawgridY], [gridX,drawgridY],3)
+        else:
+            verticalWire = True
+            pygame.draw.line(gameDisplay, (0,255,255), [drawgridX,drawgridY], [drawgridX,gridY],3)
     #render drawn lines
     if len(lines)>0:
         for l in lines :
             pygame.draw.line(gameDisplay, (0,255,255), [l[0],l[1]],[l[2],l[3]],3)
-    #pygame.draw.rect(gameDisplay, (0,255,255), [cursx-10,cursy-10,25,25])
+    #pygame.draw.rect(gameDisplay, (0,255,255), [gridX-10,gridY-10,25,25])
     #pygame.draw.rect(gameDisplay, (0,255,255), [320,130,280,170])
 
-    #pygame.draw.line(gameDisplay, (0,255,255), [0,0], [cursx,cursy])
+    #pygame.draw.line(gameDisplay, (0,255,255), [0,0], [gridX,gridY])
     pygame.display.update()
 pygame.quit()
