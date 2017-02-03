b10fe4a11991aa4630d424c97b0d72d1d4d4fffa
Ahmed Nematallah <ahmed_nematallah@hotmail.com>
2017-02-03 12:12:12 +0200

    Cleaned code

--- Circuit Viewer.py
+++ Circuit Viewer.py
@@ -1,34 +1,37 @@
-import pygame , Buttons
+"""This is the circuit viewer module."""
+import pygame
+import Buttons
 import copy
 import enum
-import sys
+# import sys
 import struct
-import ctypes
+# import ctypes
 import numpy as np
 import matplotlib.pyplot as plt
 from pygame.locals import *
-import pygame, sys, eztext
+import eztext
 import time
 eventType = enum.Enum("eventType", "Quit Mouse_Motion Key_Down Mouse_Up")
 
-
 def snapToGrid(x, y, gridspace):
+	"""Snap cursor to grid."""
 	if (x % gridspace < gridspace / 2):
 		x = x - (x % gridspace)
 	else:
-		x = (x + gridspace)  - (x % gridspace)
+		x = (x + gridspace) - (x % gridspace)
 	if (y % gridspace < gridspace / 2):
 		y = y - (y % gridspace)
 	else:
 		y = (y + gridspace) - (y % gridspace)
-	return x,y
+	return x, y
 
 def flatten(listOfLists):
-	"Flatten one level of nesting"
+	"""Flatten one level of nesting."""
 	z = [x for sublist in listOfLists for x in sublist]
 	return z
 
 def flattenml(listOfLists):
+	"""Flatten multiple levels of nesting."""
 	listOfLists2 = copy.deepcopy(listOfLists)
 	while not(type(listOfLists2[0]) == int):
 		listOfLists3 = flatten(listOfLists2)
@@ -40,16 +43,18 @@ def flattenml(listOfLists):
 global font
 pygame.font.init()
 font = pygame.font.SysFont(None, 25)
-def writeonscreen(text,color,pos):
-	screentext = font.render(text,True,color)
-	gameDisplay.blit(screentext,pos)
+
+def writeonscreen(text, color, pos):
+	screentext = font.render(text, True, color)
+	gameDisplay.blit(screentext, pos)
+
 # create Buttons
-buttonnamelist=["Resistor","Capacitor","Inductor","Diode","Voltage source","Current source","Ground","conductance","OPAMP"]
+buttonnamelist = ["Resistor", "Capacitor", "Inductor", "Diode", "Voltage source", "Current source", "Ground", "conductance", "OPAMP"]
 buttonlist = []
-#for b in buttonnamelist:
- #   button = Buttons.Button()
-#	buttonlist.append(button)
-	
+# for b in buttonnamelist:
+# button = Buttons.Button()
+# buttonlist.append(button)
+
 resistor = Buttons.Button()
 capacitor = Buttons.Button()
 inductor = Buttons.Button()
@@ -67,6 +72,7 @@ DC = Buttons.Button()
 sweep = Buttons.Button()
 
 def initalize():
+	"""Initialize SDL and other stuff."""
 	global gameDisplay
 	global clock
 	global gridspace
@@ -76,7 +82,7 @@ def initalize():
 	global compdict
 	global typedict
 	global currentComponent
-	#load component icons
+	# load component icons
 	resistoricon = pygame.image.load('Resources/res.png')
 	resistoricon = pygame.transform.rotate(resistoricon, 90)
 	capacitoricon = pygame.image.load('Resources/Capacitor_Symbol.png')
@@ -91,35 +97,33 @@ def initalize():
 	opampicon = pygame.image.load('Resources/Op-amp_symbol.png')
 	opampicon = pygame.transform.rotate(opampicon, 90)
 
-
-	print (resistoricon.get_rect().size[1]) # you can get size
+	print (resistoricon.get_rect().size[1])  # you can get size
 	# , draw coordinates,width and length,start and end point in grid coordinates, color , type
-	#compdict = {"R":[0,-25,-5,50,10,1,0,-1,0,(255,150,60),"R"],"C":[0,-25,-8,50,16,1,0,-1,0,(200,150,200),"C"]
-	#,"V":[0,-25,-10,50,20,1,0,-1,0,(255,0,0),"V"],"G":[0,-25,-10,50,20,1,0,1,0,(0,0,0),"G"]}
-	
-#create buttons
+	# compdict = {"R":[0,-25,-5,50,10,1,0,-1,0,(255,150,60),"R"],"C":[0,-25,-8,50,16,1,0,-1,0,(200,150,200),"C"]
+	# ,"V":[0,-25,-10,50,20,1,0,-1,0,(255,0,0),"V"],"G":[0,-25,-10,50,20,1,0,1,0,(0,0,0),"G"]}
 
-	#create component dictionary
-	compdict = {0:None,1:resistoricon,2:capacitoricon,3:inductoricon,4:diodeicon,5:vsourceicon,6:csourceicon,7:gndicon,8:resistoricon,9:opampicon}
-	typedict={1:'R',2:'C',3:'L',4:'D',5:'V',6:'I',7:'G',8:'g',9:'O'}
+# create buttons
+
+	# create component dictionary
+	compdict = {0:None, 1:resistoricon, 2:capacitoricon, 3:inductoricon, 4:diodeicon, 5:vsourceicon, 6:csourceicon, 7:gndicon, 8:resistoricon, 9:opampicon}
+	typedict = {1 : 'R', 2 : 'C', 3 : 'L', 4 : 'D', 5 : 'V', 6 : 'I', 7 : 'G', 8 : 'g', 9 : 'O'}
 	pygame.init()
 	clock = pygame.time.Clock()
-	gameDisplay = pygame.display.set_mode((1000,720))
+	gameDisplay = pygame.display.set_mode((1000, 720))
 	pygame.display.set_caption("cool circuit simulator 2000 😎 Now showing : " + title)
 	gridspace = 25
 	linethickness = 3
-	lineColor = (0,255,255)
-	backgroundColor = (150,150,150)
-	#res =pygame.image.load(fileobj, namehint="")
+	lineColor = (0, 255, 255)
+	backgroundColor = (150, 150, 150)
+	# res =pygame.image.load(fileobj, namehint="")
 
 def render():
+	"""Render the scene."""
 	global currentComponent
 	global componentOrientationRender
 	gameDisplay.fill(backgroundColor)
-
-#Displaying buttons
-		#Parameters:           surface,      color,       x,   y,   length, height, width,    text,      text_color
-
+	# Displaying buttons
+	# Parameters:           surface,      color,       x,   y,   length, height, width,    text,      text_color
 	resistor.create_button(   gameDisplay, (160,160,160), 0 , 0   , 200    ,    40 ,    0, "Resistor", (0,0,0))
 	capacitor.create_button(  gameDisplay, (160,160,160), 200 , 0   , 200    ,    40 ,    0, "Capacitor", (0,0,0))
 	inductor.create_button(   gameDisplay, (160,160,160), 400 , 0   , 200    ,    40 ,    0, "Inductor", (0,0,0))
@@ -135,8 +139,8 @@ def render():
 	AC.create_button(         gameDisplay, (160,160,160), 400 , 80   , 200    ,    40 ,    0, "AC", (0,0,0))
 	DC.create_button(         gameDisplay, (160,160,160), 600 , 80   , 200    ,    40 ,    0, "DC", (0,0,0))
 	sweep.create_button(      gameDisplay, (160,160,160), 800 , 80   , 200    ,    40 ,    0, "Sweep", (0,0,0))
-	
-#Render components/wires currently being edited
+
+	# Render components/wires currently being edited
 	if drawingLine:
 		if abs(initialCoordinates[0] - gridCoordinates[0]) >= abs(initialCoordinates[1] - gridCoordinates[1]):
 			componentOrientationRender = 0
@@ -148,65 +152,65 @@ def render():
 	if drawingComponenet:
 		compheight = compdict[currentComponent].get_rect().size[0]
 		if componentOrientationRender == 0:
-			gameDisplay.blit(compdict[currentComponent],(gridCoordinates[0]-(compheight/2)+1,gridCoordinates[1]))
+			gameDisplay.blit(compdict[currentComponent], (gridCoordinates[0] - (compheight / 2) + 1, gridCoordinates[1]))
 		if componentOrientationRender == 1:
 			compimage = pygame.transform.rotate(compdict[currentComponent], 270)
-			gameDisplay.blit(compimage,(gridCoordinates[0],gridCoordinates[1]-(compheight/2)+1))
+			gameDisplay.blit(compimage, (gridCoordinates[0], gridCoordinates[1] - (compheight / 2) + 1))
 
-
-#Render components
+# Render components
 	if len(components) > 0:
 		for c in components:
 			if c[1] == 1:
 				if c[0] == 0:
-					pygame.draw.line(gameDisplay, (0,0,255), [c[2],c[3]], [c[2]+c[5],c[3]],2)
-				elif c[0]!= 0 :
+					pygame.draw.line(gameDisplay, (0, 0, 255), [c[2], c[3]], [c[2] + c[5], c[3]], 2)
+				elif c[0] != 0:
 					compheight = compdict[c[0]].get_rect().size[0]
 					compimage = compdict[c[0]]
 					compimage = pygame.transform.rotate(compimage, 90)
-					gameDisplay.blit(compimage,(c[2],c[3]-(compheight/2)+1))
-					writeonscreen(c[4],(0,255,0),[c[2],c[3]-30])
-					writeonscreen("value "+str(c[5]),(0,255,0),[c[2],c[3]+30])
-			elif c[1]==0:
+					gameDisplay.blit(compimage, (c[2], c[3] - (compheight / 2) + 1))
+					writeonscreen(c[4], (0, 255, 0), [c[2], c[3] - 30])
+					writeonscreen("value " + str(c[5]), (0, 255, 0), [c[2], c[3] + 30])
+			elif c[1] == 0:
 				if c[0] == 0:
-					pygame.draw.line(gameDisplay, (0,0,255), [c[2],c[3]], [c[2],c[3]+c[5]],2)
-				elif c[0]!= 0 :
+					pygame.draw.line(gameDisplay, (0, 0, 255), [c[2], c[3]], [c[2], c[3] + c[5]], 2)
+				elif c[0] != 0:
 					compheight = compdict[c[0]].get_rect().size[0]
 					compimage = compdict[c[0]]
-					gameDisplay.blit(compimage,(c[2]-(compheight/2)+1,c[3]))
-					writeonscreen(c[4],(0,255,0),[c[2]-30,c[3]])
-					writeonscreen("value "+str(c[5]),(0,255,0),[c[2]+30,c[3]])
-			elif c[1]==2:
+					gameDisplay.blit(compimage, (c[2] - (compheight / 2) + 1, c[3]))
+					writeonscreen(c[4], (0, 255, 0), [c[2] - 30, c[3]])
+					writeonscreen("value " + str(c[5]), (0, 255, 0), [c[2] + 30, c[3]])
+			elif c[1] == 2:
 				if c[0] == 0:
-					pygame.draw.line(gameDisplay, (0,0,255), [c[2],c[3]], [c[2],c[3]+c[5]],2)
-				elif c[0]!= 0 :
+					pygame.draw.line(gameDisplay, (0, 0, 255), [c[2], c[3]], [c[2], c[3] + c[5]], 2)
+				elif c[0] != 0:
 					compheight = compdict[c[0]].get_rect().size[0]
 					compimage = compdict[c[0]]
 					compimage = pygame.transform.rotate(compimage, 180)
-					gameDisplay.blit(compimage,(c[2]-(compheight/2)+1,c[3]))
-					writeonscreen(c[4],(0,255,0),[c[2]-30,c[3]])
-					writeonscreen("value "+str(c[5]),(0,255,0),[c[2]+30,c[3]])
-			elif c[1]==3:
+					gameDisplay.blit(compimage, (c[2] - (compheight / 2) + 1, c[3]))
+					writeonscreen(c[4], (0, 255, 0), [c[2] - 30, c[3]])
+					writeonscreen("value " + str(c[5]), (0, 255, 0), [c[2] + 30, c[3]])
+			elif c[1] == 3:
 				if c[0] == 0:
-					pygame.draw.line(gameDisplay, (0,0,255), [c[2],c[3]], [c[2],c[3]+c[5]],2)
-				elif c[0]!= 0 :
+					pygame.draw.line(gameDisplay, (0, 0, 255), [c[2], c[3]], [c[2], c[3] + c[5]], 2)
+				elif c[0] != 0:
 					compheight = compdict[c[0]].get_rect().size[0]
 					compimage = compdict[c[0]]
 					compimage = pygame.transform.rotate(compimage, 270)
-					gameDisplay.blit(compimage,(c[2],c[3]-(compheight/2)+1))
-					writeonscreen(c[4],(0,255,0),[c[2],c[3]-30])
-					writeonscreen("value "+str(c[5]),(0,255,0),[c[2],c[3]+30])
-				#pygame.draw.rect(gameDisplay, (0,255,0), [c[2],c[3],100,20])
+					gameDisplay.blit(compimage, (c[2], c[3] - (compheight / 2) + 1))
+					writeonscreen(c[4], (0, 255, 0), [c[2], c[3] - 30])
+					writeonscreen("value " + str(c[5]), (0, 255, 0), [c[2], c[3] + 30])
+				# pygame.draw.rect(gameDisplay, (0,255,0), [c[2],c[3],100,20])
 
-		#pygame.draw.rect(gameDisplay, i[4], flatten(i)[0:4])
+		# pygame.draw.rect(gameDisplay, i[4], flatten(i)[0:4])
 	pygame.display.update()
 
 def checkEvents():
+	"""Check Pygame Events."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			return eventType.Quit, None
 		elif event.type == pygame.MOUSEMOTION:
-			#print(event.pos)
+			# print(event.pos)
 			return eventType.Mouse_Motion, event.pos
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_r:
@@ -277,6 +281,7 @@ def checkEvents():
 	return 0, 0
 
 def loadFile(fileName):
+	"""Load a file into the simulator."""
 	global title
 	global components
 	f = open(fileName, 'rb')
@@ -296,7 +301,7 @@ def loadFile(fileName):
 		counter += 1
 
 	componentCount = int.from_bytes(data[counter:counter + 8], 'big')
-	#print(componentCount)
+	# print(componentCount)
 	counter += 8
 	reservedBits = int.from_bytes(data[counter:counter + 4], 'big')
 	counter += 4 + reservedBits
@@ -319,9 +324,9 @@ def loadFile(fileName):
 
 		componentValue = struct.unpack('!d', data[counter:counter + 8])[0]
 		counter += 8
-		print(componentType,componentOrientation,componentCoordx,componentCoordy,componentName,componentValue)
+		print(componentType, componentOrientation, componentCoordx, componentCoordy, componentName, componentValue)
 		print('-----')
-		components.append([componentType,componentOrientation,componentCoordx,componentCoordy,componentName,componentValue])
+		components.append([componentType, componentOrientation, componentCoordx, componentCoordy, componentName, componentValue])
 		print (components)
 		componentReserveLength = int.from_bytes(data[counter:counter + 4], 'big')
 		counter += 4 + componentReserveLength
@@ -329,6 +334,7 @@ def loadFile(fileName):
 	return 0
 
 def saveFile(fileName):
+	"""Save current circuit to file."""
 	f = open(fileName, 'wb+')
 	f.write(bytes("CIR0", 'ascii'))
 	for i in range(16):
@@ -356,6 +362,7 @@ def saveFile(fileName):
 	f.close()
 
 def generateNetlist():
+	"""Generate netlist for simulation."""
 	nodes = [0 for i in components]
 	connections = [[] for i in components]
 	netlist = ""
@@ -367,7 +374,7 @@ def generateNetlist():
 		if nodes[i] == 0:
 			nodes[i] = i
 			connnod = findConnectedNodes(i, nodes)
-			if not(connnod == None):
+			if not(connnod is None):
 				for j in connnod:
 					nodes[j] = i
 
@@ -413,11 +420,12 @@ def generateNetlist():
 						if(findCollisionWirePoint(components[j], [components[i][2] + 100, components[i][3]])):
 							connections[i].append("T3")
 							connections[i].append(nodes[j])
-	
+
 	print(nodes)
 	print(connections)
 
 def findConnectedNodes(indexw1, mask):
+	"""Find all nodes connected to a wire."""
 	rrrrrr = []
 	mmmmm = copy.copy(mask)
 	for i in range(len(components)):
@@ -441,6 +449,7 @@ def findConnectedNodes(indexw1, mask):
 
 
 def findWireCollision(wire1, wire2):
+	"""Find if one wire ends on another."""
 	if (findCollisionWirePoint(wire1, [wire2[2], wire2[3]])):
 		return True
 	if (findCollisionWirePoint(wire1, [wire2[2] + wire2[5], wire2[3]]) & ~wire1[1]):
@@ -456,6 +465,7 @@ def findWireCollision(wire1, wire2):
 	return False
 
 def findCollisionWirePoint(wire, point):
+	"""Find if a point lies on a wire."""
 	if (wire[5] > 0):
 		if((point[0] >= wire[2]) & (point[0] <= wire[2] + wire[5]) & (point[1] == wire[3]) & (wire[1])):
 			return True
@@ -469,49 +479,52 @@ def findCollisionWirePoint(wire, point):
 
 	return False
 
-
-
 def detectCollision(component, Coordinates):
+	"""Find if coordinates are on a component."""
 	global compdict
 	compheight = 100
 	compwidth = 100
 	if(component[1] == 0):
-		#compheight = compdict[c[0]].get_rect().size[1]
-		#compwidth = compdict[c[0]].get_rect().size[0]
+		# compheight = compdict[c[0]].get_rect().size[1]
+		# compwidth = compdict[c[0]].get_rect().size[0]
 		if ((Coordinates[0] > c[2] - compwidth / 2) & (Coordinates[0] < (c[2] + compwidth / 2)) & 
 			(Coordinates[1] > c[3]) & (Coordinates[1] < (c[3] + compheight))):
 			return True
 		else:
 			return False
 	else:
-	# 	compheight = compdict[c[0]].get_rect().size[0]
-	# 	compwidth = compdict[c[0]].get_rect().size[1]
+		# compheight = compdict[c[0]].get_rect().size[0]
+		# compwidth = compdict[c[0]].get_rect().size[1]
 		if ((Coordinates[0] > c[2]) & (Coordinates[0] < (c[2] + compwidth)) & 
 			(Coordinates[1] > c[3] - compheight / 2) & (Coordinates[1] < (c[3] + compheight / 2))):
 			return True
 		else:
 			return False
-		
 
 	return False
+
 def AC_analysis():
+	"""Perform AC Analysis."""
 	print("AC analysis")
 	pass
 
 def DC_analysis():
+	"""Perform DC Analysis."""
 	print("DC analysis")
 	pass
 
 def sweep_analysis():
+	"""Perform Sweep Analysis."""
 	print("sweep analysis")
 	pass
 
 def kill():
+	"""End Circuit Simulator."""
 	saveFile("myfirstcir.cir")
 	pygame.quit()
 	quit()
 
-#variables
+# variables
 
 # clock = 0
 # gameDisplay = None
@@ -519,7 +532,7 @@ killApp = False
 drawingLine = False
 drawingComponenet = False
 initialCoordinates = [0, 0]
-componentOrientationRender = 0 #0->H 1->v
+componentOrientationRender = 0  # 0->H 1->v
 gridCoordinates = [0, 0]
 lines = []
 verticalWire = False
@@ -530,17 +543,17 @@ if(loadFile("myfirstcir.cir") == -1):
 
 initalize()
 
-#main loop
+# main loop
 while not killApp:
 	time.sleep(0.025)
 	returnedEvent, eventParameter = checkEvents()
 	if (returnedEvent == eventType.Quit):
 		killApp = True
-#update grid coordinates every time the mouse moves
+# update grid coordinates every time the mouse moves
 	elif (returnedEvent == eventType.Mouse_Motion):
-		gridCoordinates = [snapToGrid(eventParameter[0], eventParameter[1],25)[0],snapToGrid(eventParameter[0], eventParameter[1],25)[1]]
-		#print(gridCoordinates)
-#if key pressed start drawing a component
+		gridCoordinates = [snapToGrid(eventParameter[0], eventParameter[1], 25)[0], snapToGrid(eventParameter[0], eventParameter[1], 25)[1]]
+		# print(gridCoordinates)
+# if key pressed start drawing a component
 	elif (returnedEvent == eventType.Key_Down):
 		if (eventParameter == "r"):
 			drawingLine = False
@@ -588,7 +601,7 @@ while not killApp:
 			componentOrientationRender = 0
 			drawingComponenet = not drawingComponenet
 		if (eventParameter == "s"):
-			writeonscreen("saved",(255,0,0),[750,550])
+			writeonscreen("saved", (255, 0, 0), [750, 550])
 			pass
 		if (eventParameter == "n"):
 			generateNetlist()
@@ -602,46 +615,48 @@ while not killApp:
 			for c in components:
 				if detectCollision(c, gridCoordinates):
 					components.remove(c)
-				elif gridCoordinates == [c[2],c[3]]:
+				elif gridCoordinates == [c[2], c[3]]:
 					components.remove(c)
 		elif (eventParameter == "q"):
 			componentOrientationRender = not componentOrientationRender
-#when mouse up
+	# when mouse up
 	elif (returnedEvent == eventType.Mouse_Up):
-#start drawing lines
+		# start drawing lines
 		if ((drawingLine == False) & (drawingComponenet == False)):
 			initialCoordinates = copy.copy(gridCoordinates)
 			drawingLine = True
-#save drawn line
+		# save drawn line
 		elif drawingLine == True:
 			linecount = 0
 			for c in components:
 				if c[4][0]=='N':
 					linecount+=1
 			if (componentOrientationRender):
-				components.append([0,0,initialCoordinates[0],initialCoordinates[1],("N"+str(linecount)),gridCoordinates[1]-initialCoordinates[1]])
+				components.append([0, 0, initialCoordinates[0], initialCoordinates[1], ("N" + str(linecount)), 
+									gridCoordinates[1] - initialCoordinates[1]])
 				print(components)
 				print("check")
 			else:
-				components.append([0,1,initialCoordinates[0],initialCoordinates[1],("N"+str(linecount)),gridCoordinates[0]-initialCoordinates[0]])
+				components.append([0, 1, initialCoordinates[0], initialCoordinates[1], ("N" + str(linecount)), 
+									gridCoordinates[0] - initialCoordinates[0]])
 				print(components)
 			drawingLine = False
-#or save drawn component
+		# or save drawn component
 		elif drawingComponenet == True:
 			compcount = 0
 			for c in components:
-				if c[4][0]==typedict[currentComponent]:
-					compcount+=1
+				if c[4][0] == typedict[currentComponent]:
+					compcount += 1
 			drawingComponenet = False
 			if componentOrientationRender == 0:
 				finished = 0
-				txtbx = eztext.Input(maxlength=45, color=(0,255,0), prompt='type value here: ')
+				txtbx = eztext.Input(maxlength=45, color=(0, 255, 0), prompt='type value here: ')
 				while not finished:
 					# update txtbx
 					clock.tick(60)
 					events = pygame.event.get()
-					val =txtbx.update(events)
-					if val != None :
+					val = txtbx.update(events)
+					if val != None:
 						finished = 1
 					# blit txtbx on the sceen
 					txtbx.draw(gameDisplay)
@@ -652,7 +667,7 @@ while not killApp:
 			if componentOrientationRender == 1:
 				events = pygame.event.get()
 				finished = 0
-				txtbx = eztext.Input(maxlength=45, color=(255,0,0), prompt='type value here: ')
+				txtbx = eztext.Input(maxlength=45, color=(255, 0, 0), prompt='type value here: ')
 				while not finished:
 					# update txtbx
 					clock.tick(60)
